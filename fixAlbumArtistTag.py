8e39e21a2b947f73e15b0c6cecebba417f1022e8
pudgenuts <pjohnson21211@gmail.com>
2021-01-14 17:51:59 -0500

    cleaned up code a little

--- fixAlbumArtistTag.py
+++ fixAlbumArtistTag.py
@@ -4,23 +4,26 @@ import eyed3
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--file')
+parser.add_argument('--fix')
+parser.add_argument('--artist')
 args = parser.parse_args()
 
 tag = TinyTag.get(args.file)
 
 if tag.albumartist is None: 
-    print("tag.albumartist is None")
-    AlbumArtist = tag.artist
-    print(tag.artist)
-    print("----------------------------------")
+    print("\ttag.albumartist is None")
+    AlbumArtist = tag.artist.strip()
+    if args.fix is not None:
+        print("\t\tupdating album artist to: %s for file: %s" % (AlbumArtist, args.file))
+        audiofile = eyed3.load(args.file)
+        audiofile.tag.album_artist = AlbumArtist
+        audiofile.tag.save() 
+        tag = TinyTag.get(args.file)
+        if tag.albumartist is None:
+            print("\talbumartist updated to: %s" % (tag.albumartist))
+        else:
+            print("\tERROR updating %s" % (args.file))
 
-    audiofile = eyed3.load(args.file)
-    audiofile.tag.album_artist = AlbumArtist
-    audiofile.tag.save() 
-    print("----------------------------------")
-
-    tag = TinyTag.get(args.file)
-    print(tag.albumartist)
 else: 
     print("albumartist is set to: %s" % (tag.albumartist))
     
